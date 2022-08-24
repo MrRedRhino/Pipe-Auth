3ea5d93c6fbb29203e7c60e23d333c26b0fd7925
MrRedRhino <80647470+MrRedRhino@users.noreply.github.com>
2022-08-24 23:50:23 +0200

    Huh, this stuff didn't get committed

--- src/main/java/org/pipeman/pa/login/Api.java
+++ src/main/java/org/pipeman/pa/login/Api.java
@@ -1,20 +1,27 @@
 package org.pipeman.pa.login;
 
+import io.github.bucket4j.Bucket;
 import org.pipeman.pa.Main;
+import org.pipeman.pa.RatelimitTest;
 import org.pipeman.pa.permissions.May;
 import org.pipeman.pa.users.User;
 import org.pipeman.pa.users.Users;
 import org.rapidoid.http.Req;
 import org.rapidoid.http.Resp;
 
+import java.net.URLDecoder;
+import java.nio.charset.Charset;
+import java.time.Duration;
 import java.util.Map;
 import java.util.Random;
+import java.util.StringJoiner;
 
 public class Api {
     private static final Random random = new Random();
+    private static final RatelimitTest ratelimit = new RatelimitTest();
 
     public static Object isAuthorized(Req req, Resp resp) throws InterruptedException {
-        String path = req.header("X-Original-URI", null);
+        String path = sanitizeUrl(req.header("X-Original-URI", null));
         String domain = req.header("X-Original-Domain", null);
         String token = req.cookie("token", req.header("token", null));
 
@@ -50,6 +57,9 @@ public class Api {
     }
 
     public static Object login(Req req, Resp resp) throws InterruptedException {
+        Bucket bucket = ratelimit.resolveBucket(req.header("X-Real-IP", req.clientIpAddress()));
+        if (!bucket.asBlocking().tryConsume(1, Duration.ofSeconds(1))) return return429(resp);
+
         Thread.sleep(500 + random.nextInt(-100, 100));
         String name = req.header("name", null);
         String password = req.header("password", null);
@@ -83,10 +93,41 @@ public class Api {
         return resp.code(authorized ? 200 : 401).json(Map.of("authorized", authorized));
     }
 
+    private static Resp return429(Resp resp) {
+        return resp.code(429).plain("");
+    }
+
     private static Resp loginReturn(Resp resp, boolean authorized, String token) {
         return resp.code(authorized ? 200 : 401).json(Map.of(
                 "authorized", authorized,
                 "token", token == null || token.isBlank() ? "" : token
         ));
+    }
+
+    private static String sanitizeUrl(String url) {
+        String decoded = URLDecoder.decode(url, Charset.defaultCharset());
+        String[] pathParts = decoded.split("/");
+        String[] out = new String[pathParts.length];
+        int idx = 0;
+
+        for (String pathPart : pathParts) {
+            if (pathPart.equals(".") || pathPart.isEmpty()) continue;
+
+            if (pathPart.equals("..")) {
+                idx--;
+                if (idx >= 0) out[idx] = null;
+            } else {
+                if (idx >= 0) out[idx] = pathPart;
+                idx++;
+            }
+        }
+
+        StringJoiner joiner = new StringJoiner("/");
+        for (String s : out) {
+            if (s != null) joiner.add(s);
+        }
+
+        if (joiner.length() == 0) return "/";
+        return "/" + (decoded.charAt(decoded.length() - 1) == '/' ? joiner + "/" : joiner.toString());
     }
 }
